d74f134fe2e141d633f0d2898bd31d133c1d3376
whozwho <whozwho@chimpanzee.esquimalt.no-ip.org>
2012-08-20 22:41:43 -0700

    Fix sample TEMPLATE_DIRS.

--- samples/settings.py
+++ samples/settings.py
@@ -133,7 +133,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'home/crlb/bin/django/esquimalt/WhoZwho/templates/',
+    '/home/my_id/django/my_project/WhoZwho/templates/',
 )
 
 INSTALLED_APPS = (
