f2f9f0519d465c4260e4266f7d3c433d9e1fbd4a
Colin Leavett-Brown <crlb@telus.net>
2013-11-03 06:40:27 -0800

    Add tiles: to directory list.

--- static/js/DirectoryList.js
+++ static/js/DirectoryList.js
@@ -4,33 +4,13 @@ You may distribute under the terms of either the GNU General Public
 License or the Apache v2 License, as specified in the README file.
 */
 
-function DisplayHideLists(my) {
-    var children, firstnode, i;
-
-    if (my == null) {
-        firstnode=document.getElementById("F");
-        if (firstnode.style.display == 'none') {
-            firstnode=document.getElementById("L");
-            }
-        DisplayHideLists(firstnode);
-        }
-    else {
-        if (my.tagName == "DIV" && my.id.length > 1) {
-            my.style.display = 'none';
-            }
-
-        children = my.childNodes;
-        for (i=0;i<children.length;i++) {
-            DisplayHideLists(children[i]);
-            }
-        }
-    }
-
-function DisplaySelectedList(id) {
+function DirectoryListABC(opt) {
     var dlist_parm, p1, p2, p3;
 
-    DisplayHideLists();
-    document.getElementById(id).style.display = 'block';
+    p1 = opt.substring(0,1);
+
+    SelectHTMLdiv('i' + p1, 'i' + opt, 'text_selected', 'text_deselected');
+    SelectHTMLdiv('l' + p1, 'l' + opt);
 
     dlist_parm = getCookie("dlist_parm");
     if (dlist_parm === undefined) {
@@ -41,42 +21,28 @@ function DisplaySelectedList(id) {
         p3 = dlist_parm.substring(2,3);
         }
 
-    p1 = id.substring(0,1);
-
     if (p1 == "F") {
-        p2 = id.substring(1,2);
+        p2 = opt.substring(1,2);
         }
     else {
-        p3 = id.substring(1,2);
+        p3 = opt.substring(1,2);
         }
 
     setCookie("dlist_parm", p1 + p2 + p3);
     }
 
-function DisplaySwitchFirstLast() {
-    var dlist_parm, first, last, p1, p2;
+function DirectoryListFLMW(opt) {
+    var dlist_parm, p2;
+
+    SelectHTMLdiv('subtabs', 'subtabs' + opt, 'subtabs_selected', 'subtabs_deselected');
+    SelectHTMLdiv('List', 'List' + opt);
 
     dlist_parm = getCookie("dlist_parm");
     if (dlist_parm === undefined) {
         p2 = '..';
-        }
-    else {
+    } else {
         p2 = dlist_parm.substring(1,3);
-        }
-
-    first = document.getElementById('F');
-    last = document.getElementById('L');
-
-    if (first.style.display == '' || first.style.display == 'block') {
-        first.style.display = 'none';
-        last.style.display = 'block';
-        p1 = 'L';
-        }
-    else {
-        first.style.display = 'block';
-        last.style.display = 'none';
-        p1 = 'F';
-        }
-
-    setCookie("dlist_parm", p1 + p2);
     }
+
+    setCookie("dlist_parm", opt + p2);
+}
